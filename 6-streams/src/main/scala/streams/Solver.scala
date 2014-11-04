4848a1873b88abe0ef82fbcee567d91b91b6cb94
Louis Pilfold <louis@lpil.uk>
2014-11-04 13:33:22 +0000

    6 streams: neighbors and from

--- 6-streams/src/main/scala/streams/Solver.scala
+++ 6-streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,9 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b match {
+    case Block(b1, b2) => b1 == goal && b2 == goal
+  }
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +30,10 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(
+    b: Block, history: List[Move]): Stream[(Block, List[Move])] = for {
+      position <- b.legalNeighbors.toStream
+    } yield (position._1, (position._2 :: history))
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +41,9 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+    explored: Set[Block]): Stream[(Block, List[Move])] = for {
+      neighbor <- neighbors if !explored.contains(neighbor._1)
+    } yield neighbor
 
   /**
    * The function `from` returns the stream of all possible paths
--- 6-streams/src/test/scala/streams/BloxorzSuite.scala
+++ 6-streams/src/test/scala/streams/BloxorzSuite.scala
@@ -122,7 +122,6 @@ class BloxorzSuite extends FunSuite {
         (Block( Pos(0,1),Pos(0,2) ), Right),
         (Block( Pos(1,0),Pos(2,0) ), Down)
       ))
-
       assert(b2.neighbors === List(
         (Block( Pos(2,0),Pos(2,1) ), Left),
         (Block( Pos(2,3),Pos(2,4) ), Right),
@@ -132,13 +131,49 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
-  ignore("optimal solution for level 1") {
+  // Solver done
+  test("Solver done") {
+    new Level1 {
+      assert( done(Block(goal, goal)), "done Block(goal, goal)")
+    }
+  }
+
+  // Solver neighborsWithHistory
+  test("Solver neighborsWithHistory") {
+    new Level1 {
+      assert(
+        neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet
+        === Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        )
+      )
+    }
+  }
+
+  // Solver newNeighborsOnly
+  test("Solver newNeighborsOnly") {
+    new Level1 {
+      assert(
+        newNeighborsOnly(
+          Set(
+            (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+            (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+            ).toStream,
+          Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+        )
+        === Set((Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))).toStream
+      )
+    }
+  }
+
+  test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
     }
   }
 
-  ignore("optimal solution length for level 1") {
+  test("optimal solution length for level 1") {
     new Level1 {
       assert(solution.length == optsolution.length)
     }
